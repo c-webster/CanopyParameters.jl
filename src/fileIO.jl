ab3bc6cb7237d25fc71e4b74a3f2dc5278768ed6
Clare Webster <clare.s.webster@gmail.com>
2022-09-16 12:11:36 +0200

    nodatval added
    no data value added to output files

--- src/fileIO.jl
+++ src/fileIO.jl
@@ -8,14 +8,14 @@ function createfiles_calcCP(outdir::String,outstr::String,pts::Array{Float64,2},
         defVar(ds,"easting",pts[:,1],("Coordinates",))
         defVar(ds,"northing",pts[:,2],("Coordinates",))
 
-        cc5   = defVar(ds,"CanopyCover_5m",Int32,("Coordinates",),deflatelevel=5,attrib=["scale_factor"=>0.01,])
-        cc50  = defVar(ds,"CanopyCover_50m",Int32,("Coordinates",),deflatelevel=5,attrib=["scale_factor"=>0.01,])
-        mCH5  = defVar(ds,"MeanCanopyHeight_5m",Int32,("Coordinates",),deflatelevel=5,attrib=["scale_factor"=>0.01,])
-        mCH50 = defVar(ds,"MeanCanopyHeight_50m",Int32,("Coordinates",),deflatelevel=5,attrib=["scale_factor"=>0.01,])
+        cc5   = defVar(ds,"CanopyCover_5m",Int32,("Coordinates",),deflatelevel=5,fillvalue = Int32(-9999),attrib=["scale_factor"=>0.01,])
+        cc50  = defVar(ds,"CanopyCover_50m",Int32,("Coordinates",),deflatelevel=5,fillvalue = Int32(-9999),attrib=["scale_factor"=>0.01,])
+        mCH5  = defVar(ds,"MeanCanopyHeight_5m",Int32,("Coordinates",),deflatelevel=5,fillvalue = Int32(-9999),attrib=["scale_factor"=>0.01,])
+        mCH50 = defVar(ds,"MeanCanopyHeight_50m",Int32,("Coordinates",),deflatelevel=5,fillvalue = Int32(-9999),attrib=["scale_factor"=>0.01,])
 
         if OSHD
-            mLA5  = defVar(ds,"MeanLeafArea_5m",Int32,("Coordinates",),deflatelevel=5,attrib=["scale_factor"=>0.01,])
-            mLA50 = defVar(ds,"MeanLeafAreA_50m",Int32,("Coordinates",),deflatelevel=5,attrib=["scale_factor"=>0.01,])
+            mLA5  = defVar(ds,"MeanLeafArea_5m",Int32,("Coordinates",),deflatelevel=5,fillvalue = Int32(-9999),attrib=["scale_factor"=>0.01,])
+            mLA50 = defVar(ds,"MeanLeafArea_50m",Int32,("Coordinates",),deflatelevel=5,fillvalue = Int32(-9999),attrib=["scale_factor"=>0.01,])
             return cc5,cc50,mCH5,mCH50,mLA5,mLA50, ds
         else
             return cc5,cc50,mCH5,mCH50, ds
