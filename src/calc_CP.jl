fa3313080be31849262d8ee441aff2f71cc4a813
c-webster <clare.s.webster@gmail.com>
2023-10-31 11:56:06 +0100

    update calcCP_forFSM

--- src/calc_CP.jl
+++ src/calc_CP.jl
@@ -49,14 +49,11 @@ function calc_CP_forOSHD(chm_x::Vector{Float64},chm_y::Vector{Float64},chm_z::Ve
 end
 
 
-function calc_CP_forFSM(fname::String,pts::Matrix{Float64},outf::String,
-            can_local::Float64,can_stand::Float64,height_cutoff=2.0::Float64)
+function calc_CP_forFSM(chm_x::Vector{Float64},chm_y::Vector{Float64},chm_z::Vector{Float64},
+    pts_x::Vector{Float64},pts_y::Vector{Float64},exdir::String,outstr::String,height_cutoff=2::Number)
 
-    pts_x = pts[:,1]
-    pts_y = pts[:,2]
-
-    # load the chm
-    chm_x, chm_y, chm_z = read_griddata(fname,true,true); #vectorized
+    can_local = 5.0
+    can_stand = 50.0
 
     chm_b = fill(NaN,size(chm_z))
     chm_b[chm_z .< height_cutoff] .= 0
@@ -64,34 +61,37 @@ function calc_CP_forFSM(fname::String,pts::Matrix{Float64},outf::String,
 
     chm_bool = Bool.(chm_b)
 
-    cc5   = Vector{Float64}();
-    cc50  = Vector{Float64}();
-    mCH5  = Vector{Float64}();
-    mCH50 = Vector{Float64}();
+    cc5,cc50,mCH5,mCH50, dataset = createfiles_calcCP(exdir,outstr,hcat(pts_x,pts_y),false)
 
     for px in eachindex(pts_x)
 
         dist = hypot.(chm_x.-pts_x[px],chm_y.-pts_y[px])
 
         dx_local = dist .< can_local
+
+        if sum(dx_local) > 0 && !isempty(chm_z[dx_local .& chm_bool])
+            cc5[px]  = Int.(round.(mean(chm_b[dx_local]).*100))
+            mCH5[px] = Int.(round.(mean(chm_z[dx_local .& chm_bool]).*100))
+        else
+            cc5[px]  = 0
+            mCH5[px] = 0
+        end
+
         dx_stand = dist .< can_stand
 
-        push!(cc5,mean(chm_b[dx_local]))
-        push!(cc50,mean(chm_b[dx_stand]))
-
-        push!(mCH5,mean(chm_z[dx_local .& chm_bool]))
-        push!(mCH50,mean(chm_z[dx_stand .& chm_bool]))
+        if sum(dx_stand) > 0 && !isempty(chm_z[dx_stand .& chm_bool])
+            cc50[px]  = Int.(round.(mean(chm_b[dx_stand]).*100))
+            mCH50[px] = Int.(round.(mean(chm_z[dx_stand .& chm_bool]).*100))
+        else
+            cc50[px] = 0
+            mCH50[px] = 0
+        end
 
     end
 
-    header = ["easting" "northing" "CC5" "CC50" "mCH5" "mCH50"]
+    close(dataset)
 
-    open(outf;write=true) do f
-        writedlm(f,header)
-    end
-    f = open(outf,"a")
-        writedlm(f,hcat(pts_x,pts_y,cc5,cc50,mCH5,mCH50))
-    close(f)
+
 
 
 end
